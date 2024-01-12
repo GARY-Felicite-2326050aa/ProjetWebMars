83634e10a28ca7a98ee0a7a80139b5138ce46727
Félicité Gary <146103677+felicitegary@users.noreply.github.com>
2024-01-12 20:07:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,8 +83,21 @@ window.onload = function() {
   var styleSauvegarde = localStorage.getItem("styleChoisi");
   var lienFeuilleStyle = document.getElementById("style");
 
+  var pageActuelle = window.location.href;
+  var nomFichier = pageActuelle.substring(pageActuelle.lastIndexOf("/") + 1);
+
   if (styleSauvegarde && lienFeuilleStyle) {
-    lienFeuilleStyle.setAttribute("href", styleSauvegarde);
+    if (styleSauvegarde === "../css/style.css" && nomFichier === "index.html") {
+      lienFeuilleStyle.setAttribute("href", "/css/style.css");
+    } else if (styleSauvegarde === "../css/style2.css" && nomFichier === "index.html") {
+      lienFeuilleStyle.setAttribute("href", "/css/style2.css");
+    } else if (styleSauvegarde === "/css/style.css" && nomFichier != "index.html") {
+      lienFeuilleStyle.setAttribute("href", "../css/style.css");
+    } else if (styleSauvegarde === "/css/style2.css" && nomFichier != "index.html") {
+      lienFeuilleStyle.setAttribute("href", "../css/style2.css");
+    } else {
+      lienFeuilleStyle.setAttribute("href", styleSauvegarde);
+    }
   }
 };
 
