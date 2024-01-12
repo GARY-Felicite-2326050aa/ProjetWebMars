0a37adf5ed7e06a75a5651569a1b4bf045848548
Félicité Gary <146103677+felicitegary@users.noreply.github.com>
2024-01-12 22:10:40 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,16 +82,17 @@ function changerFeuilleStyle() {
 
 
 window.onload = function() {
+  sleep(1000);
   var styleSauvegarde = localStorage.getItem("styleChoisi");
   var lienFeuilleStyle = document.getElementById("style");
 
   var pageActuelle = window.location.href;
   var nomFichier = pageActuelle.substring(pageActuelle.lastIndexOf("/") + 1);
 
-  if (styleSauvegarde && lienFeuilleStyle) {
-    if (styleSauvegarde === "../css/style.css" && (nomFichier === "index.html" || nomFichier === null)) {
+  if (styleSauvegarde !== null && lienFeuilleStyle !== null) {
+    if (styleSauvegarde === "../css/style.css" && (nomFichier === "index.html" || nomFichier === "")) {
       lienFeuilleStyle.setAttribute("href", "./css/style.css");
-    } else if (styleSauvegarde === "../css/style2.css" &&  (nomFichier === "index.html" || nomFichier === null)) {
+    } else if (styleSauvegarde === "../css/style2.css" &&  (nomFichier === "index.html" || nomFichier === "")) {
       lienFeuilleStyle.setAttribute("href", "./css/style2.css");
     } else if (styleSauvegarde === "./css/style.css" && nomFichier !== "index.html") {
       lienFeuilleStyle.setAttribute("href", "../css/style.css");
